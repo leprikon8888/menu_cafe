2846fd2f6a0c7b07113f30c51eadfb552dacc371
leprikon8888 <131337892+leprikon8888@users.noreply.github.com>
2023-08-21 18:06:05 +0300

    Delete silver_discount.py

--- silver_discount.py
+++
@@ -1,7 +0,0 @@
-from discount import Discount
-
-class SilverDiscount(Discount):
-    def __init__(self, value =7):
-        super().__init__(value)
-    def discount(self):
-        return  1 -self.value/100
