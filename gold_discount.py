c43c20ecf6b4d450a237e4094f3270cdc9da94d4
leprikon8888 <131337892+leprikon8888@users.noreply.github.com>
2023-08-21 18:06:14 +0300

    Delete gold_discount.py

--- gold_discount.py
+++
@@ -1,7 +0,0 @@
-from discount import Discount
-
-class GoldDiscount(Discount):
-    def __init__(self, value =10):
-        super().__init__(value)
-    def discount (self):
-        return  1 -self.value/100
