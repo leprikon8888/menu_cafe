69db213826c92784014ab08b010e124d76207098
leprikon8888 <leprikon8888@gmail.com>
2023-08-21 18:04:17 +0300

    add_classes(Discount)

--- discount.py
+++ discount.py
@@ -4,3 +4,24 @@ class Discount:
 
     def discount(self):
         raise  NotImplementedError
+
+
+class RegularDiscount(Discount):
+    def __init__(self, value =3):
+        super().__init__(value)
+    def discount(self):
+        return  1 -self.value/100
+
+
+class SilverDiscount(Discount):
+    def __init__(self, value =7):
+        super().__init__(value)
+    def discount(self):
+        return  1 -self.value/100
+
+
+class GoldDiscount(Discount):
+    def __init__(self, value =10):
+        super().__init__(value)
+    def discount (self):
+        return  1 -self.value/100
